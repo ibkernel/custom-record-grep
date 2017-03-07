872076b2d46a9b84e796904ba2a672419d18217a
CWaYNe <magedesang@gmail.com>
2017-03-07 17:38:09 +0800

    Added book tag info extractor

--- data/bookFormatter.py
+++ data/bookFormatter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re, os, signal, sys
+from collections import deque
 ''' 這個檔案是用來合併下載完的章節，並加上額外delimeter(tags)好讓搜尋更為準確
 		delimeter detail:
 			<tag+tags num> 
@@ -99,97 +100,169 @@ class BookFormater:
 	non_sense = ["請記住本站域名: 黃金屋", "\"", "”","“", " "]
 
 	def __init__(self):
+		self.already_merged = []
 		pass
 
 	def mergeChaptersIntoBooks(self):
 		to_merge_titles = self.getNotMergeBookList()
+		merged_list_file = open(BookFormater.path_to_merged_file, 'a')
 		for book_title in to_merge_titles:
 			self.merge(book_title)
+			merged_list_file.write("{}\n".format(book_title))
+		merged_list_file.close()
 
 	'''讀dwlist.txt，除了最後一本書，還有可能沒下載全，不進行合併'''
 	def getNotMergeBookList(self):
 
-		# TODO:read already_merged.txt
-
+		merged_list_file = open(BookFormater.path_to_merged_file, 'r')
+		for line in merged_list_file:
+			self.already_merged.append(line.strip("\n"))
+		merged_list_file.close()
 		# read dwlist.txt
 		downloaded_book_title = []
 		download_list_file = open(BookFormater.path_to_downloaded_list, 'r')
 		for line in download_list_file:
-			downloaded_book_title.append(line.split("\t")[0])
+			book_title = line.split("\t")[0]
+			if not book_title in self.already_merged:
+				downloaded_book_title.append(line.split("\t")[0])
 		download_list_file.close()
 
 		return downloaded_book_title
 
-
+#TODO: add no-tag version of the function -> we already have locations
 	def merge(self, book_title):
 	#def format(self, text): # Just for local testing
 		#text = u"{}".format(text.decode('utf-8'))
 		source_path = BookFormater.path_to_download_dir + book_title
 		destination_path = BookFormater.path_to_formatted_dir + book_title + '.txt'
 		formatted_text = ""
-		b_num, c_num, t_num, p_num, l_num = 1,1,0,0,0
+		b_num, c_num, t_num, p_num, s_num = 1,1,0,0,0
+		char_count = 0;
 
 		to_merge_chapters_path = [source_path+'/'+f for f in os.listdir(source_path+'/') if os.path.isfile(source_path+'/'+f)]
 
 		destination_file = open(destination_path, 'w')
-		# initial setup for destination file
-		destination_file.write('@id:{}\n'.format(book_title))
-		destination_file.write('@title:{}\n'.format(book_title))
+		
+		id_meta_content = "@id:{}\n".format(book_title)
+		title_meta_content = "@title:{}\n".format(book_title)
+		char_count += len(id_meta_content)
+		char_count += len(title_meta_content)
+		char_count += len("@content:")
+		destination_file.write('{}'.format(id_meta_content))
+		destination_file.write('{}'.format(title_meta_content))
 		destination_file.write('@content:')
+
 		formatted_text += "<b"+str(b_num)+">"
+		char_count += len(formatted_text)
 		print('Merging {}'.format(book_title))
 		for chapter_path in to_merge_chapters_path:
 			chapter_file = open(chapter_path, 'r')
 			print('Current chapter: {}'.format(chapter_file))
+
+			tag_queue = deque()
+
 			text = ""
 			for line in chapter_file:
 				text += line
 			for nonsense in BookFormater.non_sense:
 				#text = text.replace(nonsense.decode('utf-8'), "")
 				text = text.replace(nonsense, "")
-			title_flag = True
+
+			tag_queue.append(("c_"+str(c_num), char_count+1))
 			formatted_text += "<c"+str(c_num)+">"
+			char_count += len("<c"+str(c_num)+">")
+			title_flag = True
 			for line in text.split("\n"):
 				if line:
 					if not title_flag:
 						p_num += 1
+						tag_queue.append(("p_"+str(p_num), char_count+1))
+						char_count += len("<p_"+str(p_num)+">")
 						formatted_text += ("<p_"+str(p_num)+">")
-						line , l_num = self.lineFormatter(line, l_num)
+
+						line , s_num, char_count, tag_queue = self.lineFormatter(line, s_num, char_count, tag_queue)
 						formatted_text += line
+
+						tag_queue.append(("p_"+str(p_num), char_count+1))
+						char_count += len("</p_"+str(p_num)+">")
 						formatted_text += ("</p_"+str(p_num)+">")
 						#print("{}".format(line.encode('utf-8')))
 					else:
 						t_num += 1
+						tag_queue.append(("t_"+str(t_num), char_count+1))
+						char_count += len("<t_"+str(t_num)+">")
 						formatted_text += ("<t_"+str(t_num)+">")
-						formatted_text += line 
+						char_count += len(line)
+						formatted_text += line
+						tag_queue.append(("t_"+str(t_num), char_count+1))
+						char_count += len("</t_"+str(t_num)+">")
 						formatted_text += ("</t_"+str(t_num)+">")
 						title_flag = False
+			tag_queue.append(("c_"+str(c_num), char_count+1))
+			char_count += len("</c"+str(c_num)+">")
 			formatted_text += "</c"+str(c_num)+">"
 			c_num += 1
 			chapter_file.close()
 			destination_file.write(formatted_text)
 			formatted_text = ""
+			self.writeTagInfoToFile(tag_queue, book_title)
 			
 		formatted_text += "</b"+str(b_num)+">"
+		destination_file.write(formatted_text)
 		destination_file.write('\n')
 		destination_file.close()
 		print('End merging {}'.format(book_title))
 		#return formatted_text
 		#print(formatted_text)
 
-	def lineFormatter(self, line_content, l_num):
+	def writeTagInfoToFile(self, tag_queue, book_title):
+		tag_info_path = BookFormater.path_to_formatted_dir + book_title + '.info'
+		tag_file = open(tag_info_path, 'a')
+
+
+		open_chapter_tag, open_tag_location = tag_queue.popleft()
+		close_chapter_tag, close_tag_location  = tag_queue.pop()
+		tag_file.write("{}\t{}\t{}\n".format(open_chapter_tag, open_tag_location, close_tag_location))
+		open_title_tag, open_tag_location = tag_queue.popleft()
+		close_title_tag, close_tag_location  = tag_queue.popleft()
+		tag_file.write("{}\t{}\t{}\n".format(open_title_tag, open_tag_location, close_tag_location))
+
+
+		other_tag_queue, queue_count = deque(), 0
+		for tag, tag_location in tag_queue:
+			if not other_tag_queue:
+				other_tag_queue.append((tag, tag_location))
+			elif tag in other_tag_queue[0]:
+				_ , location = other_tag_queue.popleft()
+				tag_file.write("{}\t{}\t{}\n".format(tag, location, tag_location))
+				for _ in range(len(other_tag_queue)//2):
+					open_tag, open_tag_location = other_tag_queue.popleft()
+					close_tag, close_tag_location = other_tag_queue.popleft()
+					tag_file.write("{}\t{}\t{}\n".format(open_tag, open_tag_location, close_tag_location))
+			else:
+				other_tag_queue.append((tag, tag_location))
+		tag_file.close()
+
+	def lineFormatter(self, line_content, s_num, char_count, tag_queue):
 		# find ? 。 . ... !
 		pattern = re.compile('[!?。？！。]|(\.+)')
 		new_content_list = re.split(pattern, line_content)
 		processed_content = ""
 		for content in new_content_list:
 			if content:
-				l_num += 1
-				processed_content += ("<l_"+str(l_num)+">")
-				processed_content += content
-				processed_content += ("</l_"+str(l_num)+">")
+				s_num += 1
+				tag_queue.append(("s_"+str(s_num), char_count+1))
+				char_count += len("<s_"+str(s_num)+">")
+				processed_content += ("<s_"+str(s_num)+">")
 
-		return processed_content, l_num
+				char_count += len(content)
+				processed_content += content
+
+				tag_queue.append(("s_"+str(s_num), char_count+1))
+				char_count += len("</s_"+str(s_num)+">")
+				processed_content += ("</s_"+str(s_num)+">")
+
+		return processed_content, s_num, char_count, tag_queue
 
 
 print (sys.version)
